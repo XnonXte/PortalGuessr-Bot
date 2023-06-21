d8df7518ecc9a371348c62e7aef71e13e8e500ee
Quddus Salam <quddussalam555@gmail.com>
2023-06-21 17:33:44 +0700

    Update v0.3.1-beta
    Refer to changelogs for more information about this update.

--- components/guessr.py
+++ components/guessr.py
@@ -1,37 +1,52 @@
 import os
 import random
 import json
-
-chambers_list = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "e00",
-    "e01",
-    "e02",
-]
+import discord
 
 
 class Guessr:
+    chambers_list = [
+        "01",
+        "02",
+        "03",
+        "04",
+        "05",
+        "06",
+        "07",
+        "08",
+        "09",
+        "10",
+        "11",
+        "12",
+        "13",
+        "14",
+        "15",
+        "16",
+        "17",
+        "18",
+        "19",
+        "e00",
+        "e01",
+        "e02",
+    ]
+
+    def __init__(self):
+        self.difficulty_colors = {
+            "Easy": discord.Color.from_rgb(46, 139, 87),
+            "Medium": discord.Color.from_rgb(204, 204, 0),
+            "Hard": discord.Color.from_rgb(178, 34, 34),
+        }
+        self.difficulty_timeouts = {
+            "Easy": 20,
+            "Medium": 25,
+            "Hard": 30,
+        }
+
     def get_guess(self, difficulty):
         """Get a guessr question."""
-        random_chamber = random.choice(chambers_list)
+        random_chamber = random.choice(Guessr.chambers_list)
+        difficulty_color = self.difficulty_colors.get(difficulty)
+        difficulty_timeout = self.difficulty_timeouts.get(difficulty)
 
         image_path = f"resources/images/{difficulty}/{random_chamber}"
         image_files = [
@@ -45,18 +60,23 @@ class Guessr:
             image_path, image_files[random_image_index]
         )  # Gets the path for the random image.
 
-        return random_image_path, random_chamber
+        return (
+            random_image_path,
+            random_chamber,
+            difficulty_color,
+            difficulty_timeout,
+        )
 
 
-class GuessrUsersLeaderboard:
-    """Class to handle the users' leaderboard. If filename doesn't exist, it will create it automatically."""
+class GuessrUsersStatistics:
+    """Class to handle the users' statistics. If filename doesn't exist, it will create it automatically."""
 
     def __init__(self, filepath):
         self.filepath = filepath
         self.stats = {}
 
     def load_stats(self):
-        """Load the users' stats from the leaderboard."""
+        """Load the users' stats from the statistics."""
         try:
             with open(self.filepath, "r") as file:
                 self.stats = json.load(file)
@@ -67,31 +87,54 @@ class GuessrUsersLeaderboard:
                 return self.stats
 
     def save_stats(self):
-        """Save the users' stats to the leaderboard."""
+        """Save the users' stats to the statistics."""
         with open(self.filepath, "w") as file:
             json.dump(self.stats, file)
 
-    def add_user_stats(self, user_id, difficulty):
-        """Add a user's stats to the leaderboard."""
-        if str(user_id) not in self.stats:
-            self.stats[str(user_id)] = {"Easy": 0, "Medium": 0, "Hard": 0}
+    def add_user_stats(self, server_id, user_id, difficulty):
+        """Add a user's stats to the statistics."""
+        server_id = str(server_id)
+        user_id = str(user_id)
 
-        self.stats[str(user_id)][difficulty] += 1
+        if server_id not in self.stats:
+            self.stats[server_id] = {}
+        if user_id not in self.stats[server_id]:
+            self.stats[server_id][user_id] = {"Easy": 0, "Medium": 0, "Hard": 0}
+        self.stats[server_id][user_id][difficulty] += 1
 
-    def get_sorted_stats(self):
+    def get_sorted_stats(self, server_id):
         """Returns a sorted list of users' stats in descending order"""
-        return sorted(
-            self.stats.items(), key=lambda x: sum(x[1].values()), reverse=True
+        server_id = str(server_id)
+        if server_id not in self.stats:
+            raise KeyError("Server not found in statistics.")
+        sorted_statistics = sorted(
+            self.stats[server_id].items(),
+            key=lambda x: sum(x[1].values()),
+            reverse=True,
         )
+        return sorted_statistics
 
-    def delete_user_stats(self, user_id):
-        """Delete a user's stats from the leaderboard"""
+    def get_user_stats(self, server_id, user_id):
+        """Returns a user's stats"""
+        server_id = str(server_id)
+        user_id = str(user_id)
+        if server_id not in self.stats:
+            raise KeyError("Server not found in statistics.")
+        if user_id not in self.stats[server_id]:
+            raise KeyError("User not found in statistics.")
+        return self.stats[server_id][user_id]
+
+    def delete_user_stats(self, server_id, user_id):
+        """Delete a user's stats from the statistics"""
+        server_id = str(server_id)
+        user_id = str(user_id)
+        if server_id not in self.stats:
+            raise KeyError("Server not found in statistics.")
         key_to_remove = None
-        for key in self.stats:
-            if str(user_id) == key:
+        for key in self.stats[server_id]:
+            if user_id == key:
                 key_to_remove = key
-
         if key_to_remove:
             del self.stats[key_to_remove]
         else:
-            raise KeyError("User not found in leaderboard.")
+            raise KeyError("User not found in statistics.")
